54e8b9cecc6d780d37737f6ad81502fcc2834e44
iorichina <iorichina@163.com>
2024-09-12 16:03:41 +0000

    中间件
    Signed-off-by: iorichina <iorichina@163.com>

--- solution/tcp_middleware.go
+++ solution/tcp_middleware.go
@@ -16,26 +16,98 @@ func tcpClient() (net.Conn, error) {
 }
 
 func serverProcess(logger *log.Logger, conn net.Conn) {
+	now := time.Now().Format("2006-01-02 15:04:05.000")
 	var mac string
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
 			now := time.Now().Format("2006-01-02 15:04:05.000")
-			logger.Printf("%v[%v][%v] Close err %v\t%#v\n", now, conn.RemoteAddr(), mac, err.Error(), err)
+			logger.Printf("%v[%v][%v]server Close err %v\t%#v\n", now, conn.RemoteAddr(), mac, err.Error(), err)
 		}
 	}(conn) // 关闭连接
-	remote := conn.RemoteAddr()
-	now := time.Now().Format("2006-01-02 15:04:05.000")
-	logger.Printf("%v[%v] Connected to %v\n", now, remote, conn.LocalAddr())
+	now = time.Now().Format("2006-01-02 15:04:05.000")
+	logger.Printf("%v[%v]Connected to %v\n", now, conn.RemoteAddr(), conn.LocalAddr())
 
 	var err error
 	client, err := tcpClient()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			now := time.Now().Format("2006-01-02 15:04:05.000")
+			logger.Printf("%v[%v][%v]client Close err %v\t%#v\n", now, conn.RemoteAddr(), mac, err.Error(), err)
+		}
+	}(client) // 关闭连接
+	now = time.Now().Format("2006-01-02 15:04:05.000")
+	logger.Printf("%v[%v]Connected by %v\n", now, client.RemoteAddr(), client.LocalAddr())
 
 	_, err = conn.Write([]byte{254, 134, 226, 1, 121, 29, 9, 52, 61})
+	now = time.Now().Format("2006-01-02 15:04:05.000")
 	if err != nil {
-		logger.Printf("%v[%v](0x34) Write err %v\n", now, remote, err)
+		logger.Printf("%v[%v]server Write(0x34) err %v\n", now, conn.RemoteAddr(), err)
+		return
 	}
 
+	closeChan := make(chan error)
+	defer close(closeChan)
+	connChan := make(chan error)
+	defer close(connChan)
+	clientChan := make(chan error)
+	defer close(clientChan)
+	macChan := make(chan string)
+	defer close(macChan)
+	go func(macChan chan string) {
+		for m := range macChan {
+			mac = m
+		}
+	}(macChan)
+	go handleServer(logger, macChan, conn, client, closeChan)
+	go handleClient(logger, conn, connChan, client, clientChnn)
+	err = <- closeChan
+}
+
+func handleClient(logger *log.Logger, conn net.Conn, client net.Conn, closeChan chan error) {
+	remote := client.RemoteAddr()
+	now := time.Now().Format("2006-01-02 15:04:05.000")
+	var mac string
+	var err error
+	var n int
+	reader := bufio.NewReader(client)
+	var buf [512]byte
+	for {
+		now = time.Now().Format("2006-01-02 15:04:05.000")
+		n, err = reader.Read(buf[:]) // 读取数据
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				logger.Printf("%v[%v][%v]client Read EOF %#v\n", now, remote, mac, err)
+			} else {
+				logger.Printf("%v[%v][%v]client Read failed err %v\t%#v\n", now, remote, mac, err.Error(), err)
+			}
+			closeChan <- err
+			break
+		}
+
+		now = time.Now().Format("2006-01-02 15:04:05.000")
+		if n > 8 && 0xFE == buf[0] && 0x01 == buf[3] {
+			if 0x31 == buf[7] {
+				logger.Printf("%v[%v][%v]client 读 %#x\t% X\n", now, remote, mac, buf[7], buf[:n])
+			}
+		}
+
+		_, err = conn.Write(buf[:n])
+		if err != nil {
+			now = time.Now().Format("2006-01-02 15:04:05.000")
+			logger.Printf("%v[%v][%v]server Write err %v\t%#v\n", now, conn.RemoteAddr(), mac, err.Error(), err)
+			closeChan <- err
+			break
+		} // 发送数据
+	}
+}
+
+func handleServer(logger *log.Logger, macChan chan string, conn net.Conn, client net.Conn, closeChan chan error) {
+	remote := conn.RemoteAddr()
+	now := time.Now().Format("2006-01-02 15:04:05.000")
+	var mac string
+	var err error
 	var n int
 	reader := bufio.NewReader(conn)
 	var buf [512]byte
@@ -44,10 +116,11 @@ func serverProcess(logger *log.Logger, conn net.Conn) {
 		n, err = reader.Read(buf[:]) // 读取数据
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				logger.Printf("%v[%v][%v]Read EOF %#v\n", now, remote, mac, err)
+				logger.Printf("%v[%v][%v]server Read EOF %#v\n", now, remote, mac, err)
 			} else {
-				logger.Printf("%v[%v][%v]Read failed err %v\t%#v\n", now, remote, mac, err.Error(), err)
+				logger.Printf("%v[%v][%v]server Read failed err %v\t%#v\n", now, remote, mac, err.Error(), err)
 			}
+			closeChan <- err
 			break
 		}
 
@@ -55,19 +128,23 @@ func serverProcess(logger *log.Logger, conn net.Conn) {
 		if n > 8 && 0xFE == buf[0] && 0x01 == buf[3] {
 			if 0x34 == buf[7] {
 				mac = strings.Join([]string{string(buf[9:11]), string(buf[11:13]), string(buf[13:15]), string(buf[15:17]), string(buf[17:19]), string(buf[19:21])}, ":")
+				macChan <- mac
 			}
 			if 0x35 == buf[7] {
 				mac = strings.Join([]string{string(buf[8:10]), string(buf[10:12]), string(buf[12:14]), string(buf[14:16]), string(buf[16:18]), string(buf[18:20])}, ":")
-				logger.Printf("%v[%v][%v]读 %#x 心跳\n", now, remote, mac, buf[7])
+				logger.Printf("%v[%v][%v]server 读 %#x 心跳\n", now, remote, mac, buf[7])
+				macChan <- mac
 			}
 			if 0x30 == buf[7] || 0x3b == buf[7] {
-				logger.Printf("%v[%v][%v]读 %#x\t% X\n", now, remote, mac, buf[7], buf[:n])
+				logger.Printf("%v[%v][%v]server 读 %#x\t% X\n", now, remote, mac, buf[7], buf[:n])
 			}
 		}
 
 		_, err = client.Write(buf[:n])
 		if err != nil {
-			logger.Printf("%v[%v][%v] Write err %v\n", now, remote, mac, err)
+			now = time.Now().Format("2006-01-02 15:04:05.000")
+			logger.Printf("%v[%v][%v]client Write err %v\t%#v\n", now, client.RemoteAddr(), mac, err.Error(), err)
+			closeChan <- err
 			break
 		} // 发送数据
 	}
