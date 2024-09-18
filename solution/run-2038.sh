47f2998450e56cb3262a70efa4f638494c710e6e
iorichina <iorichina@163.com>
2024-09-18 04:56:07 +0000

    update run
    Signed-off-by: iorichina <iorichina@163.com>

--- solution/run-2038.sh
+++ solution/run-2038.sh
@@ -1,2 +1,2 @@
-./tcp_client_middleware 192.168.1.38:2038 406a7637n7.goho.co:11180 >log-2038.log 2>&1 &
+./tcp_client_middleware 192.168.1.38:2038 3000  406a7637n7.goho.co:11180  10000 >log-2038.log 2>&1 &
 
--- solution/run-2039.sh
+++ solution/run-2039.sh
@@ -1,2 +1,2 @@
-./tcp_client_middleware 192.168.1.39:2039 406a7637n7.goho.co:11180 >log-2039.log 2>&1 &
+./tcp_client_middleware 192.168.1.39:2039 3000  406a7637n7.goho.co:11180 10000 >log-2039.log 2>&1 &
 
