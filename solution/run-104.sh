4bfd17a59221538d64612b99a99634245888eda3
iorichina <iorichina@163.com>
2024-09-28 12:05:29 +0800

    update runner

--- solution/run-104.sh
+++ solution/run-104.sh
@@ -1,2 +1,2 @@
-go build tcp_client_middleware.go
-./tcp_client_middleware 192.168.1.104:2102 3000  coinpush.pokekara.com:10112 3000 >>log-104.log 2>&1 &
+go build tcp_client_middleware2.go
+./tcp_client_middleware2 192.168.1.104:2102 3000  coinpush.pokekara.com:10112 3000 >>log-104.log 2>&1 &
