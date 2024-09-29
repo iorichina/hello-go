4ebcc3496348a2f21054fa4967980faf4cd7fad9
iorichina <iorichina@163.com>
2024-09-29 06:02:44 +0000

    update 38
    Signed-off-by: iorichina <iorichina@163.com>

--- solution/supervisor-2038.sh
+++ solution/supervisor-2038.sh
@@ -5,7 +5,7 @@ go build tcp_client_middleware1.go
 while true; do
         server=`ps -ef | grep tcp_client_middleware1 | grep "192.168.1.38:2039" | grep -v grep`
         if [ ! "$server" ]; then
-            ./tcp_client_middleware1 192.168.1.38:2039 3000  127.0.0.1:80 10000 >>log-2038.log 2>&1 &
+            ./tcp_client_middleware1 192.168.1.38:2039 3000  43.163.209.214:10112 10000 >>log-2038.log 2>&1 &
             sleep 10
         fi
         sleep 5
