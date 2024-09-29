3e0d9994b9a3bb49611c3329f4d456e4c414f1bd
R740 <iorichina@163.com>
2024-09-29 18:00:17 +0800

    remote cr

--- solution/tcp_supervisor-101.sh
+++ solution/tcp_supervisor-101.sh
@@ -3,10 +3,10 @@
 go build tcp_client_middleware1.go
 
 while true; do
-        server=`ps -ef | grep tcp_client_middleware1 | grep "192.168.1.101:2102" | grep -v grep`
-        if [ ! "$server" ]; then
-            ./tcp_client_middleware1 192.168.1.101:2102 3000  coinpush.pokekara.com:10112 3000 >>log-101.log 2>&1 &
-            sleep 10
-        fi
-        sleep 1
+	server=`ps -ef | grep tcp_client_middleware1 | grep "192.168.1.101:2102" | grep -v grep`
+	if [ ! "$server" ]; then
+		./tcp_client_middleware1 192.168.1.101:2102 3000  coinpush.pokekara.com:10112 3000 >>log-101.log 2>&1 &
+		sleep 10
+	fi
+	sleep 1
 done
